85490898c71e44861a3cdd1d396ea40da6d27bcd
BrunoFaria2021 <bmc_ti@outlook.com>
2023-01-20 03:03:10 -0300

    Refatorando SharedContext

---
+++ SharedContext/Base.cs
@@ -0,0 +1,15 @@
+using System;
+using Bruno.NotificationContext;
+
+namespace Bruno.SharedContext
+{
+    public abstract class Base : Notifiable
+    {
+        public Base()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
